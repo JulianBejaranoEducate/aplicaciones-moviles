664cc469937db5fdc49e8b36f01b0af088a40268
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2026-08-04 16:23:19 -0500

    Correction - app.ts - comunications

--- clases/angular/app_comunicatios1/src/app/app.ts
+++ clases/angular/app_comunicatios1/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Son } from "./components/son/son";
 import { Father } from "./components/father/father";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Son, Father],
+  imports: [RouterOutlet, Father],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
