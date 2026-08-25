bf5e4b4d3320d247f867fcafd19d31ebee00ffca
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2026-08-24 23:05:34 -0500

    Tarea - Selector de productos

---
+++ clases/angular/selector_productos/src/app/components/tarjeta/tarjeta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-tarjeta',
+  styleUrl: './tarjeta.css',
+  templateUrl: './tarjeta.html',
+})
+export class Tarjeta {}
