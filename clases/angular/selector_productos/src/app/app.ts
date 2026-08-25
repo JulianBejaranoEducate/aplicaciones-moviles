85430d1d842879ebfe72204a13e28672360ef47e
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2026-08-24 23:21:39 -0500

    Tarea - Selector de productos - [COMPLETADA]

--- clases/angular/selector_productos/src/app/app.ts
+++ clases/angular/selector_productos/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Catalogo } from './components/catalogo/catalogo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Catalogo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('selector_productos');
 }
--- clases/angular/selector_productos/src/app/components/resumen/resumen.ts
+++ clases/angular/selector_productos/src/app/components/resumen/resumen.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, model, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
+import { Producto } from '../../models/producto';
 
 @Component({
-  imports: [],
+  imports: [DecimalPipe, FormsModule],
   selector: 'app-resumen',
   styleUrl: './resumen.css',
   templateUrl: './resumen.html',
 })
-export class Resumen {}
+export class Resumen {
+
+  public readonly producto = input.required<Producto>();
+
+  public readonly cantidad = model.required<number>();
+
+  public readonly limpiar = output<void>();
+
+  public readonly total = computed<number>(
+    () => this.producto().precio * this.cantidad()
+  );
+
+  public aumentar(): void {
+    this.cantidad.update((valor) => Math.min(valor + 1, 20));
+  }
+
+  public disminuir(): void {
+    this.cantidad.update((valor) => Math.max(valor - 1, 1));
+  }
+
+  public validarCantidad(): void {
+    const valor: number = Number(this.cantidad());
+
+    this.cantidad.set(
+      Number.isFinite(valor) ? Math.min(Math.max(Math.trunc(valor), 1), 20) : 1
+    );
+  }
+  
+}
