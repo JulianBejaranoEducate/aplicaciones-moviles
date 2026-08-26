90e9c3ccc162e8d7a0cab066b7487d02e65b8737
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2026-08-26 07:20:28 -0500

    Actualizar balota.ts - app loteria

--- clases/angular/loteria/src/app/components/balota/balota.ts
+++ clases/angular/loteria/src/app/components/balota/balota.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './balota.css',
   templateUrl: './balota.html',
 })
-export class Balota {}
+export class Balota {
+
+  @Input()
+  valor: number = 0;
+}
